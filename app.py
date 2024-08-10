4068b12508c9550fc9b13411e3923aa4ddb36c56
sarvesh kesharwani <sarveshkumar5513@gmail.com>
2024-08-10 18:24:36 +0530

    streamlit app update

--- app.py
+++ app.py
@@ -8,3 +8,8 @@ house_type = st.selectbox('Select the type of house:', ['House', 'Flat'])
 
 # Display the predicted price (placeholder)
 st.text('Price of the house would be: $XXX')  # Replace $XXX with actual prediction logic
+
+
+# Create a sidebar for navigation
+st.sidebar.title("Navigation")
+page = st.sidebar.radio("Select a page:", ["Isolation Forest", "LOF"])
