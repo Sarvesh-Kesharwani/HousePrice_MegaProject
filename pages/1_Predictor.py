434c9b60d4ce95697e9c6c3f738ea5947c428570
Sarvesh Kesharwani <sarveshkumar5513@gmail.com>
2024-08-29 19:18:26 +0530

    stated working on predictor module

--- pages/1_Predictor.py
+++ pages/1_Predictor.py
@@ -1,5 +1,5 @@
 import streamlit as st
-
+import pickle
 
 # Set the title of the app
 st.title('House Price Predictor')
@@ -9,3 +9,12 @@ house_type = st.selectbox('Select the type of house:', ['House', 'Flat'])
 
 # Display the predicted price (placeholder)
 st.text('Price of the house would be: $XXX')  # Replace $XXX with actual prediction logic
+
+
+with open('model.pkl', 'rb') as m:
+    model = pickle.load(m)
+
+with open('data.pkl', 'rb') as d:
+    data = pickle.load(d)
+
+st.dataframe(data)
