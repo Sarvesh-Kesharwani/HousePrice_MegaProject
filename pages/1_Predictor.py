343355e2ae03501e2f27f9bb9fcdee8bfaa2ecb5
Sarvesh Kesharwani <sarveshkumar5513@gmail.com>
2024-08-29 23:21:04 +0530

    created a minimal app from e2e

--- pages/1_Predictor.py
+++ pages/1_Predictor.py
@@ -5,21 +5,41 @@ import pandas as pd
 # Set the title of the app
 st.title('House Price Predictor')
 
-# Dropdown for selecting house type
-house_type = st.selectbox('Select the type of house:', ['House', 'Flat'])
-
-# Display the predicted price (placeholder)
-st.text('Price of the house would be: $XXX')  # Replace $XXX with actual prediction logic
-
-
 with open(r'model.pkl', 'rb') as m:
     model = pickle.load(m)
 
-data = pd.read_csv(r'final_data.csv')
+data = pd.read_csv(r'data.csv')
+# data.drop(['Unnamed: 0'], axis=1, inplace=True)
 st.dataframe(data)
 
 # property_type
-st.selectbox('Property Type', ['flat', 'house'])
+property_type = st.selectbox('Property Type', ['flat', 'house'])
 
 # sector
-st.selectbox('Sector', data['sector'])
+sector = st.selectbox('Sector', data['sector'].unique().tolist())
+
+# floorNum
+floorNum = st.selectbox('floorNum', data['floorNum'].unique().tolist())
+
+# sector
+# st.selectbox('Sector', data['sector'].unique().tolist())
+
+# sector
+# st.selectbox('Sector', data['sector'].unique().tolist())
+
+
+data = [[]]
+columns = ['price_per_sqft', 'floorNum',
+       'property_type', 'noOfFloor', 'SuperBuiltupArea_sqft',
+       'BuiltupArea_sqft', 'CarpetArea_sqft', 'servantRoom', 'studyRoom',
+       'poojaRoom', 'storeRoom', 'otherAdditionalRoom', 'luxury_class']
+
+# Display the predicted price (placeholder)
+if st.button('Predict'):
+    # form a dataframe
+    
+    # predict
+    predicted_price = model.predict()
+    
+    # display the price
+    st.text(predicted_price)
